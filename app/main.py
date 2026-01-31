f0fb2fd18f5c21a305efa0eab6e03de9c97607d8
missarita2012-bit <missarita2012@gmail.com>
2026-01-31 12:25:39 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
 
 app = FastAPI(title="Cinema Hub")
+
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def home():
     return {"status": "Cinema Hub Streaming Server Running"}
+
+@app.get("/watch/{file_id}", response_class=HTMLResponse)
+def watch_page(request: Request, file_id: int):
+    return templates.TemplateResponse(
+        "watch.html",
+        {"request": request, "file_id": file_id}
+    )
